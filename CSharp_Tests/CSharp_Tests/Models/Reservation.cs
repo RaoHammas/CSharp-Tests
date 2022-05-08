dd517166ce5145fa82469796b73cb15a6b409429
Rao Hammas <hammas143@gmail.com>
2022-05-08 22:44:47 +0500

    updated

---
+++ CSharp_Tests/CSharp_Tests/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharp_Tests.Models
+{
+    public class Reservation
+    {
+        public User ReservationMadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin || user == ReservationMadeBy)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+
+
+
+    } //end of class
+}
---
+++ CSharp_Tests/CSharp_Tests/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharp_Tests.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
